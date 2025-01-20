267caef32afdf3510d672da406a9cef2a2ea6ffd
thisisanro <antonromanenko@tutanota.com>
2025-01-20 17:57:09 +0000

    Add text_node_to_html_node method and some unit tests for it

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -26,6 +26,27 @@ class TestTextNode(unittest.TestCase):
         node1 = TextNode("any text", TextType.BOLD, url="http://example.com")
         node2 = TextNode("any text", TextType.BOLD)
         self.assertNotEqual(node1, node2)
+
+    def test_text_to_html_normal(self):
+        text_node = TextNode("text", TextType.NORMAL)
+        html_node = text_node_to_html_node(text_node)
+        assert html_node.tag is None
+        assert html_node.value == "text"
+        assert html_node.props is None
+
+    def test_text_to_html_link(self):
+        text_node = TextNode("text", TextType.LINK, url="google.com")
+        html_node = text_node_to_html_node(text_node)
+        assert html_node.tag == "a"
+        assert html_node.value == "text"
+        assert html_node.props == {"href": "google.com"}
+
+    def test_text_to_html_image(self):
+        text_node = TextNode("text", TextType.IMAGE, url="image.com")
+        html_node = text_node_to_html_node(text_node)
+        assert html_node.tag == "img"
+        assert html_node.value == ""
+        assert html_node.props == {"src": "image.com", "alt": "text"}
 
 if __name__ == "__main__":
     unittest.main()
